11381e03242286e0e93e9306236fe9fb94d4d7f0
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2022-09-24 13:24:29 -0700

    Update main.lua

--- ezprint/main.lua
+++ ezprint/main.lua
@@ -1,1 +1,15 @@
+	["P"] = {
+		func = function (doc, arguments, Utils, Settings, Defaults)
+			local str = "";
+			for i = 1, #arguments do
+				if i ~= 1 then
+					str = str .. "," .. arguments[i]
+				else
+					str = str .. arguments[i]
 
+				end
+			end
+			
+			return "print("..str..")";
+		end,
+	},
